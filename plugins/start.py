4652f7598122a82aec5a64dcb7c9c5bc6ba28526
HIRUWA <87813313+Dark-Knight-Hiruwa@users.noreply.github.com>
2021-08-29 10:45:42 +0530

    ❰🍁🔱  T Rex BOT  🔱🍁❱   TG

--- plugins/start.py
+++ plugins/start.py
@@ -1,3 +1,5 @@
+#Developer: Hiruwa
+
 from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
 
 
@@ -5,10 +7,10 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
+        [InlineKeyboardButton("Developer", url="https://t.me/aryan_bots")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/aryanvikash")]
+            "Report Bugs 😪", url="https://t.me/hiruwa")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"හායි <b>{message.from_user.first_name}</b>\nවැඩි විස්තර සදහා /help ලබා දෙන්න.🤭\n\nHey <b>{message.from_user.first_name}</b>\n/help for More info..🤭"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
