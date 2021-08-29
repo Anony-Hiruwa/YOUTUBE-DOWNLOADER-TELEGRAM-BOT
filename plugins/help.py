5e8c6c7fd8987027de0a275e25dce628891bf87d
HIRUWA <87813313+Dark-Knight-Hiruwa@users.noreply.github.com>
2021-08-29 10:38:36 +0530

    ❰🍁🔱  T Rex BOT  🔱🍁❱   TG

--- plugins/help.py
+++ plugins/help.py
@@ -1,7 +1,9 @@
+#Developer : Hiruwa
+
 from pyrogram import Client, Filters
 
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
+    helptxt = f"ඔබට ලබා ගත හැක්කේ යූ ටියුබ් වීඩියෝස් සහ ඕඩියෝ පමනයි කරුනාකර ප්ලේලිස්ට් ලින්ක් ලබා නොදෙන්න..\nCurrently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
     await message.reply_text(helptxt)
